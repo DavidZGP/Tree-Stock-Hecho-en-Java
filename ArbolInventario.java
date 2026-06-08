746047a969b85c6faaf22e635f6b376691be93bd
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2026-06-07 19:53:18 -0500

    Update ArbolInventario.java
    Implementación del método insertar recursivo parte 1

--- ArbolInventario.java
+++ ArbolInventario.java
@@ -4,3 +4,13 @@ public class ArbolInventario {
     public ArbolInventario() {
         this.raiz = null;
     }
+
+     // Insertar producto de forma recursiva
+    public void insertar(int id, String nombre) {
+        raiz = insertarRec(raiz, id, nombre);
+    }
+
+    private Producto insertarRec(Producto actual, int id, String nombre) {
+        if (actual == null) {
+            return new Producto(id, nombre);
+        }
